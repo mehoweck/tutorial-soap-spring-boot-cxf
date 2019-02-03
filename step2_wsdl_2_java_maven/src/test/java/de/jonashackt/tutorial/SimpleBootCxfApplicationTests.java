0ba0dd4ebed1bd0ef3aff149f22fb355e93a5a39
Michal Pieprzyca <michal.pieprzyca@soprasteria.com>
2019-02-03 13:12:03 +0100

    modify step 2 to compile and run

--- step2_wsdl_2_java_maven/src/test/java/de/jonashackt/tutorial/SimpleBootCxfApplicationTests.java
+++ step2_wsdl_2_java_maven/src/test/java/de/jonashackt/tutorial/SimpleBootCxfApplicationTests.java
@@ -2,13 +2,12 @@ package de.jonashackt.tutorial;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SimpleBootCxfApplication.class)
-@WebAppConfiguration
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class SimpleBootCxfApplicationTests {
 
 	@Test
